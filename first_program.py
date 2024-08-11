f5bac0d7cd38c83b6349fcbd01d8fa872b94ae1a
KlounOK <kloun70@gmail.com>
2024-08-11 22:57:25 +0300

    Add files via upload

---
+++ first_program.py
@@ -0,0 +1,9 @@
+print('What man do you love?')
+print('I love', input())
+name_1=input()
+name_2=input()
+print('I love', name_1, 'and', name_2, 'else!')
+first='Ahmed'
+second='lovely'
+print('This man, named', first, ',', 'is my', second, 'man!')
+
