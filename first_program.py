b3839c733c69eaa6126a0e25c62f0512ee9c32ca
KlounOK <kloun70@gmail.com>
2024-08-11 23:07:59 +0300

    Update first_program.py

--- first_program.py
+++ first_program.py
@@ -1,9 +1,11 @@
-print('What man do you love?')
-print('I love', input())
+print('What man did you meet?')
+print('His name is', input())
 name_1=input()
 name_2=input()
-print('I love', name_1, 'and', name_2, 'else!')
-first='Ahmed'
-second='lovely'
-print('This man, named', first, ',', 'is my', second, 'man!')
-
+print("I've met", name_1, 'and', name_2, 'funny!')
+first='Ivan'
+second='best'
+print('This man, named', first, ',', 'is my', second, 'friend!')
+print('Hello,', input())
+name=input()
+print('Hi,', name)
